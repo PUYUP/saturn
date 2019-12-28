ac368ad3a870ac0597b881a3c631b5f2ecb13325
PUYUP <hellopuyup@gmail.com>
2019-12-28 20:06:38 +0700

    Updated

--- saturn/settings/project.py
+++ saturn/settings/project.py
@@ -104,10 +104,7 @@ AUTO_LOGGED_IN = False
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 
 # Django Simple JWT
